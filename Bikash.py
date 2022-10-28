86b8a11ba0dc417d33a6101be1d64725b3ccc557
Bikash Halder <115603304+BikashHalderNew@users.noreply.github.com>
2022-10-28 13:36:18 +0530

    Update Bikash.py

--- Bikash.py
+++ Bikash.py
@@ -11,6 +11,12 @@ API_ID = os.environ.get("API_ID", "none")
 API_HASH = os.environ.get("API_HASH", "none") 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "none") 
 MONGO_URL = os.environ.get("MONGO_URL", "none")
+BOT_IMAGE = os.environ.get("BOT_IMAGE", "none")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "none")
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "none")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "none")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "none")
+
 
 bot = Client(
     "V_Chat_Bot" ,
